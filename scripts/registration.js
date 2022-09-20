d0cfa70869d718c279ab56761fb3ccd89ec3f991
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2022-09-20 15:24:55 -0700

    Karla-Website
    Added some bootstrap and css

---
+++ scripts/registration.js
@@ -0,0 +1,13 @@
+function Client(firstName, lastName, email, phoneNumber){
+this.firstName= firstName;
+this.lastName= lastName;
+this.email=email;
+this.phone= phoneNumber;
+}
+
+
+function init(){
+
+}
+
+window.onload=init;
